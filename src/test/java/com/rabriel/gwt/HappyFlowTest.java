1aef02651ab71cecd861c0cade55d12d169a52b2
Gabriel Deliu <dcgro@yahoo.com>
2016-11-29 03:40:38 +0000

    Added flow tests.

--- src/test/java/com/rabriel/gwt/HappyFlowTest.java
+++ src/test/java/com/rabriel/gwt/HappyFlowTest.java
@@ -1,6 +1,14 @@
 package com.rabriel.gwt;
 
 import org.junit.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static com.rabriel.gwt.GivenWhenThen.given;
 import static junit.framework.TestCase.assertEquals;
 /**
@@ -15,11 +23,11 @@ public class HappyFlowTest {
         String passedStringValue = "Test Value";
 
         given(passedIntValue).when("testing passed int value in 'when'",
-                receivedValue -> { assertEquals("given passed int value should match when received value",
-                        passedIntValue, (int) receivedValue); return true; });
+            receivedValue -> { assertEquals("given passed int value should match when received value",
+                passedIntValue, (int) receivedValue); return true; });
         given(passedStringValue).when("testing passed string value in 'when'",
-                receivedValue -> { assertEquals("given passed string value should match when received value",
-                        passedStringValue, receivedValue); return true; });
+            receivedValue -> { assertEquals("given passed string value should match when received value",
+                passedStringValue, receivedValue); return true; });
 
     }
 
@@ -28,9 +36,9 @@ public class HappyFlowTest {
         String whenValue = "Test Value";
 
         given(null)
-                .when(whenReceivedValue -> whenValue)
-                .then("testing passed string value in 'then'",thenReceivedValue ->
-                { assertEquals("when passed value should match then received value",whenValue, thenReceivedValue); });
+            .when(whenReceivedValue -> whenValue)
+            .then("testing passed string value in 'then'",thenReceivedValue ->
+            { assertEquals("when passed value should match then received value",whenValue, thenReceivedValue); });
 
     }
 
@@ -40,5 +48,28 @@ public class HappyFlowTest {
             .when("multiplying by 2", givenValue -> 2*givenValue)
             .then("value should be even", whenValue -> whenValue%2 == 0);
     }
+
+    @Test
+    public void multiTypeFlowTest(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DayOfWeek expectedDay = localDateTime.getDayOfWeek();
+
+        given(localDateTime)
+            .when("retrieving current day", date -> date.getDayOfWeek())
+            .then("days should match", day -> expectedDay == day);
+    }
+    @Test
+    public void assertFlowTest(){
+        Integer primeNumber = 17;
+        given(primeNumber)
+            .when("finding dividers naively", number -> IntStream.range(1, number+1)
+                    .boxed().filter(value -> number%value == 0).collect(Collectors.toList()))
+            .then("days should match", dividers -> {
+                assertEquals("should have two dividers", 2, dividers.size());
+                assertEquals("first divider should be 1", 1, (int) dividers.get(0));
+                assertEquals(String.format("first divider should be %d", primeNumber), primeNumber, dividers.get(1));
+            });
+    }
+
 
 }
